576eb704b55e908ce00efa4a9f1f72a5b239f205
Sultan Mukhtarov <suli419956@gmail.com>
2024-01-28 15:38:04 +0600

    at the end i find out that i ddint create a variable(переменную) sendmessage and didnt put in it.

--- src/components/messages/message.tsx
+++ src/components/messages/message.tsx
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import {Message} from "../../types";
+import React, { useEffect, useState } from 'react';
+import { Message } from "../../types";
 
 const Message: React.FC = () => {
-
     const [messages, setMessages] = useState<Message[]>([]);
     const [lastMessages, setLastMessages] = useState('');
     const [newMessage, setNewMessage] = useState('');
@@ -10,31 +9,30 @@ const Message: React.FC = () => {
 
     useEffect(() => {
         const interval = setInterval(() => {
-            fetchMessages()
-        },2000);
+            fetchMessages();
+        }, 2000);
         return () => clearInterval(interval);
     }, []);
 
     const fetchMessages = async () => {
         try {
-            const params = new URLSearchParams();
-            params.set('datetime', lastMessages)
-
             const response = await fetch(`http://146.185.154.90:8000/messages?datetime=${lastMessages}`);
-            const newMessages: Message[] = await response.json()
+            const newMessages: Message[] = await response.json();
             setMessages((prevMessages) => [...prevMessages, ...newMessages]);
             if (newMessages.length > 0) {
-                const lastMessage = newMessages[newMessages.length -1];
-                setLastMessages(lastMessage.datetime)
+                const lastMessage = newMessages[newMessages.length - 1];
+                setLastMessages(lastMessage.datetime);
             }
         } catch (error) {
-            console.error('error message');
+            console.error('Error fetching messages');
         }
+    };
 
+    const sendMessage = async () => {
         const url = 'http://146.185.154.90:8000/messages';
 
         const data = new URLSearchParams();
-        data.set('message', newMessage)
+        data.set('message', newMessage);
         data.set('author', author);
 
         try {
@@ -47,13 +45,34 @@ const Message: React.FC = () => {
             setMessages((prevMessages) => [...prevMessages, sentMessage]);
             setNewMessage('');
         } catch (err) {
-            console.error('Wrong sending message');
+            console.error('Error sending message');
         }
-
-    }
+    };
 
     return (
-
+        <>
+            <div>
+                <h1>Chat</h1>
+            </div>
+            <div>
+                {messages.map((message) => (
+                    <div key={message._id}>
+                        <p>{message.author}</p>
+                        <p>{message.datetime}</p>
+                    </div>
+                ))}
+            </div>
+            <input
+                type="text"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+            />
+            <input type="text"
+                   value={author}
+                   onChange={(e) => setAuthor(e.target.value)}
+            />
+            <button onClick={sendMessage}>Send</button>
+        </>
     );
 };
 
