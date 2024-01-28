6ee57d8053f88f043e03c5206d056cd33f92add7
Sultan Mukhtarov <suli419956@gmail.com>
2024-01-28 15:48:21 +0600

    at the end i find out that i ddint create a variable(переменную) sendmessage and didnt put in it. installed message in to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Message from "./components/messages/message.tsx";
 
 const App: React.FC = () => {
     return (
         <div>
-
+        <Message/>
         </div>
     );
 };
