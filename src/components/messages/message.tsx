89e36ecf15e1a09e13556f604c7bfae9566d7b3a
Sultan Mukhtarov <suli419956@gmail.com>
2024-01-28 13:48:37 +0600

    Now i created a new state with with last messages with empty input. here i did a cycle and used type of datetime and also cached the err.

--- src/components/messages/message.tsx
+++ src/components/messages/message.tsx
@@ -4,20 +4,26 @@ import {Message} from "../../types";
 const Message: React.FC = () => {
 
     const [messages, setMessages] = useState<Message[]>([]);
+    const [lastMessages, setLastMessages] = useState('')
 
     useEffect(() => {
         const interval = setInterval(() => {
-
+            fetchMessages()
         },2000);
-
         return () => clearInterval(interval);
-
     }, []);
 
     const fetchMessages = async () => {
         try {
-            const response = await fetch('');
-            const newMessages = Message[] = await response.json()
+            const response = await fetch(`http://146.185.154.90:8000/messages?datetime=${lastMessages}`);
+            const newMessages: Message[] = await response.json()
+            setMessages((prevMessages) => [...prevMessages, ...newMessages]);
+            if (newMessages.length > 0) {
+                const lastMessage = newMessages[newMessages.length -1];
+                setLastMessages(lastMessage.datetime)
+            }
+        } catch (error) {
+            console.error('error message');
         }
     }
 
